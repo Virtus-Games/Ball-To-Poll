96fb2fd2b8c2c0bbe607bfb9b64699441ea5702d
irfanderdiyok <irfanderdiyok@hotmail.com>
2023-06-15 20:09:52 +0300

    Remove Jujube

--- Assets/Jujube Map Editor/Core/JBlock.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JujubeMapEditor.Core {
-	using UnityEngine;
-	public abstract class JBlock : MonoBehaviour {
-		public abstract void OnBlockLoaded (JujubeRenderer renderer, JujubeBlock block);
-		public virtual string GetPaletteLabel () => "J";
-		public virtual bool AllowRotate () => true;
-	}
-}
